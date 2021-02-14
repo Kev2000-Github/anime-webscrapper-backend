4b79fe919df8a557be84fa5415b523db79c98f68
Kevin <chengkev2000@gmail.com>
2021-02-14 14:22:56 -0400

    added exclude include query url

--- src/modules/anime/anime.ts
+++ src/modules/anime/anime.ts
@@ -3,10 +3,17 @@ import { getAnimes } from './animeService';
 const router = express.Router();
 
 router.get('/anime', async (req, res) => {
-    let { page, limit } = req.query;
+    let { page, limit, exclude, include } = req.query;
     let numPage = page ? +page : 1;
     let numLimit = limit ? +limit : 30;
-    const animes = await getAnimes(numPage, numLimit);
+    function processQuery(query) {
+        query = query ? query.toString().replace(/_/g, " ") : undefined;
+        let queryArr = query ? query.split('-') : undefined;
+        return queryArr;
+    }
+    let excludeArr = processQuery(exclude);
+    let includeArr = processQuery(include);
+    const animes = await getAnimes(numPage, numLimit, excludeArr, includeArr);
     res.json(animes);
 })
 
--- src/modules/anime/animeService.ts
+++ src/modules/anime/animeService.ts
@@ -1,6 +1,10 @@
 const Anime = require('../../database/models/anime');
 
-export const getAnimes = async (page: Number, limit: Number) => {
+export const getAnimes = async (page: Number, limit: Number, exclude: Array<String> = [], include: Array<String> = []) => {
     const options = { page, limit };
-    return await Anime.paginate({}, options);
+    let query = { tags: {} };
+    if (exclude.length > 0) query['tags']["$not"] = { $in: exclude };
+    if (include.length > 0) query['tags']["$in"] = include;
+    console.log(query);
+    return await Anime.paginate(query, options);
 }
